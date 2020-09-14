723cc7c47216ae123618bb3ecc16a4ab77ee53a1
Zack Wins <el_hefe@Zacks-MBP.attlocal.net>
2020-09-14 19:50:09 -0400

    removed unused vars

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { BrowserRouter as Router, Switch, Route, Redirect, useHistory } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Computer from 'bitcoin-computer'
 import Wallet from './Wallet'
 import Chat from './Chat'
--- src/SideBar.js
+++ src/SideBar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, HashRouter } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import StartChat from './StartChat'
 
 function SideBar({ chats, computer }) {
